b8ad4f61b738c1cf3dd0120a28922c376b871464
Miguel Herreros Pinilla <mhpcalatrava@gmail.com>
2024-01-17 20:43:46 +0100

    Borrado

--- main.py
+++
@@ -1,9 +0,0 @@
-import Asistente
-
-# Ejemplo de uso
-asistente = Asistente()
-
-while True:
-    asistente.controlar_asistencia()
-    asistente.registrar_usuario()
-    asistente.salir_aplicacion()
